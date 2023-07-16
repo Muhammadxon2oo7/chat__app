a8e9c3f0793d392d07cb4e676341a5b773f57baa
Muhammadxon_2oo7 <muhammadxon0709@gmail.com>
2023-07-16 10:24:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let hoverEffectimg= document.getElementById('hover_effect')
+// let headerLi=document.getElementById('header_hover__li')
+// let homeLogo=documen.getElementById('home__logo')
+// headerLi.addEventListener('mouseenter', ()=>{ 
+//     hoverEffectimg.style.display='block'
+//     hoverEffectimg.style.zIndex='1'
+//     homeLogo.src='img/home_hover.svg'
+//     homeLogo.style.zIndex='10'
+// })
+// headerLi.addEventListener('mouseleave', ()=>{
+//     hoverEffectimg.style.display='none'
+//     homeLogo.src='img/home.svg'
+// })
